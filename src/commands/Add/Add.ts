21577994ef9f1548f7d5ae614cac4f2c4df3d046
Yitzhakpro <isako14@gmail.com>
2023-02-24 22:30:04 +0200

    added summary to add function & configurations as constant

--- src/commands/Add/Add.ts
+++ src/commands/Add/Add.ts
@@ -1,28 +1,32 @@
 import { Command, Argument } from "commander";
+import { CONFIGURATIONS } from "./constants";
 import type { Configuration } from "./types";
 
 class AddCommand {
   public command: Command;
 
   constructor() {
-    const command = new Command("add")
-      .description("add new configuration to existing project")
+    const addCommand = new Command("add")
+      .summary("add a configuration to existing project")
+      .description(
+        "Add new configuration to existing project, you may include multiple configurations, using seperated values (with space or ', ')."
+      )
       .addArgument(
-        new Argument("<configurations...>", "space seperated values").choices([
-          "husky",
-          "eslint",
-          "prettier",
-        ])
+        new Argument("<configurations...>", "Configuration name(s).").choices(
+          CONFIGURATIONS
+        )
       )
       .action((configurations: Configuration[]) => {
         this.handler(configurations);
       });
 
-    this.command = command;
+    this.command = addCommand;
   }
 
   private handler(configurations: Configuration[]) {
-    console.log(configurations);
+    configurations.forEach((configuration) => {
+      console.log(configuration);
+    });
   }
 }
 
