11312ddcdb5c134baf2db8280949377074861712
Yitzhakpro <isako14@gmail.com>
2023-02-28 00:38:10 +0200

    Moved logger to util file

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ import Prompt from "./prompt";
 
 export * from "./enviroment";
 export * from "./files";
+export * as Logger from "./logger";
 export * from "./shell";
 export * from "./strings";
 export { Prompt };
---
+++ src/utils/logger.ts
@@ -0,0 +1,37 @@
+import chalk from "chalk";
+
+export const info = (message: string) => {
+  const infoPrefix = `${chalk.bold("[")}${chalk.blue("INFO")}${chalk.bold(
+    "]"
+  )}`;
+  const formattedString = chalk.bold(message);
+
+  console.log(`${infoPrefix} ${formattedString}`);
+};
+
+export const success = (message: string) => {
+  const successPrefix = `${chalk.bold("[")}${chalk.green(
+    "SUCCESS"
+  )}${chalk.bold("]")}`;
+  const formattedString = chalk.bold(message);
+
+  console.log(`${successPrefix} ${formattedString}`);
+};
+
+export const warn = (message: string) => {
+  const warnPrefix = `${chalk.bold("[")}${chalk.yellow("WARNING")}${chalk.bold(
+    "]"
+  )}`;
+  const formattedString = chalk.bold(message);
+
+  console.log(`${warnPrefix} ${formattedString}`);
+};
+
+export const error = (message: string) => {
+  const errorPrefix = `${chalk.bold("[")}${chalk.red("ERROR")}${chalk.bold(
+    "]"
+  )}`;
+  const formattedString = chalk.bold(message);
+
+  console.log(`${errorPrefix} ${formattedString}`);
+};
