0e45fc8136054334b43893c21a4fcf0e07c318dd
Yitzhakpro <isako14@gmail.com>
2023-03-07 15:08:08 +0200

    extended runCommand util to configure stdio

--- src/utils/shell.ts
+++ src/utils/shell.ts
@@ -1,25 +1,39 @@
 import { spawn } from "child_process";
 import { removeEndNewLine } from "./strings";
+import type {
+  SpawnOptionsWithStdioTuple,
+  StdioNull,
+  StdioPipe,
+} from "child_process";
 
 export const runCommand = async (
   command: string,
-  verbose = true
+  verbose = true,
+  options?: SpawnOptionsWithStdioTuple<
+    StdioNull | StdioPipe,
+    StdioNull | StdioPipe,
+    StdioNull | StdioPipe
+  >
 ): Promise<void> => {
-  const process = spawn(command, { shell: true });
+  const process = spawn(command, { shell: true, ...options });
 
-  process.stdout.on("data", (data: any) => {
-    if (verbose) {
-      const formatedText = removeEndNewLine(data.toString());
-      console.log(formatedText);
-    }
-  });
+  if (process.stdout) {
+    process.stdout.on("data", (data: any) => {
+      if (verbose) {
+        const formatedText = removeEndNewLine(data.toString());
+        console.log(formatedText);
+      }
+    });
+  }
 
-  process.stderr.on("data", (data: any) => {
-    if (verbose) {
-      const formatedText = removeEndNewLine(data.toString());
-      console.error(formatedText);
-    }
-  });
+  if (process.stderr) {
+    process.stderr.on("data", (data: any) => {
+      if (verbose) {
+        const formatedText = removeEndNewLine(data.toString());
+        console.error(formatedText);
+      }
+    });
+  }
 
   return new Promise((resolve, reject) => {
     process.on("close", (code) => {
