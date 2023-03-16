bd91fd0df134da4700f1c6bad4f2e3de493676b0
Yitzhakpro <isako14@gmail.com>
2023-03-17 00:24:02 +0200

    implemented basic eslint config enhance according to project

--- src/logic/eslint/plugins/defaults.ts
+++ src/logic/eslint/plugins/defaults.ts
@@ -1,4 +1,44 @@
 import { EslintPlugin } from "./base";
+import type { EslintRule, RuleSeverity } from "./types";
+
+export class ImportPlugin extends EslintPlugin {
+  constructor(isReactProject = false) {
+    const importRules: EslintRule = {
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          alphabetize: { order: "asc" },
+        },
+      ],
+    };
+
+    if (isReactProject) {
+      const importOrderRuleOptions = importRules["import/order"] as [
+        RuleSeverity,
+        Record<string, any>
+      ];
+      importOrderRuleOptions[1].pathGroups = [
+        {
+          pattern: "react",
+          group: "builtin",
+          position: "before",
+        },
+      ];
+      importOrderRuleOptions[1].pathGroupsExcludedImportTypes = ["react"];
+    }
+
+    super("eslint-plugin-import", importRules, "recommended");
+  }
+}
 
 export class PrettierPlugin extends EslintPlugin {
   constructor() {
@@ -12,6 +52,19 @@ export class PrettierPlugin extends EslintPlugin {
       "recommended",
       "eslint-config-prettier",
       9999
+    );
+  }
+}
+
+export class ReactHooksPlugin extends EslintPlugin {
+  constructor() {
+    super(
+      "eslint-plugin-react-hooks",
+      {
+        "react-hooks/rules-of-hooks": "error",
+        "react-hooks/exhaustive-deps": "warn",
+      },
+      "recommended"
     );
   }
 }
--- src/logic/eslint/plugins/types.ts
+++ src/logic/eslint/plugins/types.ts
@@ -1,6 +1,6 @@
-type RuleSeverity = 0 | "off" | 1 | "warn" | 2 | "error";
+export type RuleSeverity = 0 | "off" | 1 | "warn" | 2 | "error";
 
-type EslintRuleValue = RuleSeverity | [RuleSeverity, any];
+type EslintRuleValue = RuleSeverity | [RuleSeverity, Record<string, any>];
 
 export interface EslintRule {
   [ruleName: string]: EslintRuleValue;
