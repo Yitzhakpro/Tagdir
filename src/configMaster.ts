6a270869f54086ff845ebb1feceace2aa8558586
Yitzhakpro <isako14@gmail.com>
2023-03-31 17:59:57 +0300

    changed name of cli to configmaster

--- src/configMaster.ts
+++ src/configMaster.ts
@@ -1,12 +1,12 @@
 import { Command } from 'commander';
 import { Add } from './commands';
 
-export default class Program {
+export default class ConfigMaster {
 	private readonly program: Command;
 
 	constructor() {
 		const program = new Command()
-			.name('Enhancer')
+			.name('ConfigMaster')
 			.description('CLI to add configurations to project')
 			.addCommand(Add.command);
 
